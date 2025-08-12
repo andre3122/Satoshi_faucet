ac83d8caae9a38f942612214a020a1d78765b9e9
Andik_dyy <139764520+andre3122@users.noreply.github.com>
2025-08-12 20:46:56 +0700

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -26,8 +26,8 @@ function maybeEnable(){
   elClaim.disabled = !(sec===0 && token && address.length>=3);
 }
 
-const SUPABASE_URL = 'https://YOUR-SUPABASE-PROJECT.supabase.co';
-const SUPABASE_ANON = 'YOUR-SUPABASE-ANON-KEY';
+const SUPABASE_URL = 'https://jjhlpaonnnacucjcgmrc.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqaGxwYW9ubm5hY3VjamNnbXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwMDIzODYsImV4cCI6MjA3MDU3ODM4Nn0.03N9XzY7HNYxHbVs1ac0elb27Tg8gPlMYycSFHTZHhk';
 
 async function submitClaim(){
   elClaim.disabled = true;
